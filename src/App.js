d4c53d35dfba52d6184885c8f5d3548b45de22cd
kuttralaganesh <44425346+kuttralaganesh@users.noreply.github.com>
2023-08-12 01:43:21 +0530

    modify check

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
+      <h1>hai</h1>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
       <button className="btn btn-danger">Form valitation</button>
